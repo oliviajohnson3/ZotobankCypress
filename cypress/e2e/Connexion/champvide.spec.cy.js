f773b496f489527ae27ab6ecba863499d5dfef45
oliviajohnson3 <olivia.johnson3@gmail.com>
2026-07-19 12:18:27 +0200

    fix: make HTML5 validation test language-independent

--- cypress/e2e/Connexion/champvide.spec.cy.js
+++ cypress/e2e/Connexion/champvide.spec.cy.js
@@ -1,19 +1,21 @@
 describe("Robustesse de la connexion - champs vides", () => {
   it("affiche un message de validation et reste sur /login lorsqu'on clique sur Se connecter sans saisir d'identifiants", () => {
 
-    cy.visit("/login")
+    cy.visit("/login");
 
     // Action
-    cy.get('[data-testid="signin-submit"]').click()
+    cy.get('[data-testid="signin-submit"]')
+      .click();
 
     // Vérification : le champ utilisateur est invalide
-    cy.get('[data-testid="signin-username"]').then(($input) => {
-      expect($input[0].checkValidity()).to.be.false
-      expect($input[0].validationMessage)
-        .to.equal("Veuillez renseigner ce champ.")
-    })
+    cy.get('[data-testid="signin-username"]')
+      .then(($input) => {
+        expect($input[0].checkValidity()).to.be.false;
+        expect($input[0].validationMessage).not.to.be.empty;
+      });
 
     // Vérification : l'utilisateur reste sur la page de connexion
-    cy.url().should("include", "/login")
-  })
-})
+    cy.url()
+      .should("include", "/login");
+  });
+});
